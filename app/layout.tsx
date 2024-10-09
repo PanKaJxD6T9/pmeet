61dcbb4f2e23a6e1514a5057132761227e0ea0c1
notbladeuwu@gmail.com <notbladeuwu@gmail.com>
2024-10-09 17:12:54 +0530

    meeting creation and redirection to meeting Done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -30,6 +30,7 @@ export default function RootLayout({
       }}>
         <body className={`antialiased bg-dark-2`}>
           {children}
+          <Toaster />
         </body>
       </ClerkProvider>
     </html>
--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -4,6 +4,9 @@ import React, { useState } from 'react'
 import HomeCard from './HomeCard'
 import { useRouter } from 'next/navigation';
 import MeetingModal from './MeetingModal';
+import { useUser } from '@clerk/nextjs';
+import { Call, useStreamVideoClient } from '@stream-io/video-react-sdk';
+import { useToast } from "@/hooks/use-toast"
 
 const MeetingTypeList = () => {
 
@@ -11,8 +14,60 @@ const MeetingTypeList = () => {
 
   const [meetingState, setMeetingState] = useState<'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined>(undefined);
 
-  const createMeeting = () => {
+  const {user} = useUser();
 
+  const client = useStreamVideoClient();
+
+  const [values, setValues] = useState({
+    dateTime : new Date(),
+    description : '',
+    link : '' 
+  });
+
+  const[callDatails, setCallDetails] = useState<Call>();
+
+  const { toast } = useToast()
+
+  const createMeeting = async() => {
+    if(!client || !user) return;
+
+    try{
+
+      if(!values.dateTime){
+        toast({title: "Please Select a Date and Time",})
+        return;
+      }
+
+      const id = crypto.randomUUID();
+      const call = client.call('default', id);
+
+      if(!call) throw new Error("Failed To create Call");
+
+      const startsAt = values.dateTime.toISOString() || new Date(Date.now()).toISOString();
+
+      const description = values.description || 'Instant meeting';
+
+      await call.getOrCreate({
+        data: {
+          starts_at: startsAt,
+          custom: {
+            description,
+          }
+        }
+      })
+
+      setCallDetails(call);
+
+      if(!values.description) {
+        router.push(`/meeting/${call.id}`)
+      }
+
+      toast({title: "Meeting Created",})
+
+    } catch(err){
+      console.log(err);
+      toast({title: "Failed To create Meeting",})
+    }
   }
 
   return (
