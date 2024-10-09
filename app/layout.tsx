9ca878cf5bf681fa8adc703854075e5486f615ab
notbladeuwu@gmail.com <notbladeuwu@gmail.com>
2024-10-09 11:28:50 +0530

    Structured Files and added some custom tailwind css and created Navbar and Sidebar File only

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`antialiased`}
+        className={`antialiased bg-dark-2`}
       >
         {children}
       </body>
