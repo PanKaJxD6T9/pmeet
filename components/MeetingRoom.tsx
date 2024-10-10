1f7e063269992329d76eb15de6815467f7798607
notbladeuwu@gmail.com <notbladeuwu@gmail.com>
2024-10-10 12:53:06 +0530

    Added functionality to Create a Personal Meeting Room

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -13,7 +13,7 @@ import {
     DropdownMenuTrigger,
   } from "@/components/ui/dropdown-menu"
 import { LayoutList, Users } from 'lucide-react';
-import { useSearchParams } from 'next/navigation';
+import { useRouter, useSearchParams } from 'next/navigation';
 import EndCallButton from './EndCallButton';
 import Loader from './Loader';
   
@@ -25,6 +25,7 @@ type callLayoutType = 'grid' | 'speaker-left' | 'speaker-right'
 const MeetingRoom = () => {
 
     const searchParams = useSearchParams();
+    const router = useRouter();
     const isPersonalRoom = !!searchParams.get('personal')
 
     const [layout, setLayout] = useState('speaker-left')
@@ -63,7 +64,7 @@ const MeetingRoom = () => {
         </div>
       </div>
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5 flex-wrap">
-        <CallControls />
+        <CallControls onLeave={()=>{router.push('/')}} />
         <DropdownMenu>
 
           <div className='flex items-center'>
